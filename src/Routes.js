e0ba92f35172423a702ff0680acc34d24d6af4dc
Steve M <uziandari@gmail.com>
2017-05-10 20:46:36 -0400

    added bg image.

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import AgeGate from './containers/AgeGate';
 import Beers from './containers/Beers';
+import Events from './containers/Events';
 
 import AppliedRoute from './components/AppliedRoute';
 import AuthenticatedRoute from './components/AuthenticatedRoute';
@@ -16,7 +17,7 @@ export default ({ childProps }) => (
     <AuthenticatedRoute path="/" exact component={Home} props={childProps} />
     <AuthenticatedRoute path="/beers" exact component={Beers} props={childProps} />
     <UnauthenticatedRoute path="/landing" exact component={AgeGate} props={childProps} />
-
+    <AuthenticatedRoute path="/events" exact component={Events} props={childProps} />
     {/*404 catch*/}
     <Route component={NotFound} />
   </Switch>
---
+++ src/containers/Events.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Navigation from './Navigation';
+//import Calendar from 'antd';
+
+export default class Evemts extends Component {
+  render() {
+    return (
+      <div className="events">
+        <Navigation />
+        <div className="lander">
+          <h1>Events</h1>
+            
+        </div>
+      </div>
+    );
+  }
+}
